524d10db5738e51ec436f737dea32bb1af30ce75
Juca Crispim <juca@poraodojuca.net>
2017-10-22 00:09:49 -0200

    first commit for sphinx_pdj_theme

---
+++ sphinx_pdj_theme/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+VERSION = '0.1'
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    return cur_dir
