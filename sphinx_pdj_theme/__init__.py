07939235d201b9b26654cea79576de4a55b320b7
Juca Crispim <juca@debianmental.poraodojuca.dev>
2024-11-11 00:27:32 -0300

    Refactor theme
    Remove useless stuff
    Improve dark mode toggle
    Improve mobile version
    Fix css
    Fix search

--- sphinx_pdj_theme/__init__.py
+++ sphinx_pdj_theme/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 
@@ -6,6 +8,10 @@ def get_html_theme_path():
     cur_dir = os.path.abspath(os.path.dirname(__file__))
     return cur_dir
 
+def get_locale_path():
+    return  os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
+
 
 def setup(app):
+    app.add_message_catalog('sphinx', get_locale_path())
     app.add_html_theme('sphinx_pdj_theme', get_html_theme_path())
